b8e8bdc6189e77e1fbcabe6f65000affefb8c1be
Vitor Luiz <vitor.luiz.eer@gmail.com>
2023-04-02 18:59:16 -0300

    Versão com função que leva ao whatsapp

--- javascript.js
+++ javascript.js
@@ -16,3 +16,30 @@ function finalizarPedido (tipo1, tipo2, tipo3) {
         remover.classList.remove('display-escondido')
     }
 }
+
+function realizarPedido (tipo1, tipo2, tipo3) {
+    let texto;
+    let total;
+
+    const nome= prompt("Digite seu nome!")
+    const endereco= prompt("Digite seu endereço!")
+    const food= document.querySelector(`${tipo1} .select h2`).innerHTML;
+    const drink= document.querySelector(`${tipo2} .select h2`).innerHTML;
+    const dessert= document.querySelector(`${tipo3} .select h2`).innerHTML;
+    const valor_food= document.querySelector(`${tipo1} .select h4`).innerHTML.slice(3).replace(",",".");
+    const valor_drink= document.querySelector(`${tipo2} .select h4`).innerHTML.slice(3).replace(",",".");
+    const valor_dessert= document.querySelector(`${tipo3} .select h4`).innerHTML.slice(3).replace(",",".");
+
+    total= Number(valor_food) + Number(valor_drink) + Number(valor_dessert);
+    total= total.toFixed(2)
+    texto= encodeURIComponent(`Olá, gostaria de fazer o pedido
+    -Prato: ${food}
+    -Bebida: ${drink}
+    -Sobremesa: ${dessert}
+    Total: R$ ${total}
+    
+    Nome: ${nome}
+    Endereço: ${endereco}`)
+
+    window.open("https://wa.me/55082998262078?text=" + texto)
+}
